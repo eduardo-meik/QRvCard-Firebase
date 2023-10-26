c15b7f9074497ac7d9cd6a516cefc32be501e415
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-10-26 13:41:25 -0300

    Update qrlist.py

--- app/qrlist.py
+++ app/qrlist.py
@@ -1,46 +1,32 @@
 import streamlit as st
-from firebase_admin import firestore, storage
-from datetime import timedelta
+from firebase_admin import firestore
 
 def display_list():
     st.title("QR List")
 
-    # Fetch the vCards for the authenticated user from Firestore
+    # Fetch the vCard for the authenticated user from Firestore
     db = firestore.client()
-    vcard_ref = db.collection('vcards').document(st.session_state.username).get()
-    vcards = vcard_ref.to_dict()
+    vcard_data = db.collection('vcards').document(st.session_state.username).get().to_dict()
 
-    if vcards:
-        for full_name, data in vcards.items():
-            st.write(f"Raw Data for {full_name}: {data}")  # Display raw data
-            
-            # Ensure that data is a dictionary before fetching attributes
-            if isinstance(data, dict):
-                # Display the vCard details
-                st.write(f"Name: {full_name}")
-                st.write(f"Organization: {data.get('ORG', 'N/A')}")
-                st.write(f"Role: {data.get('ROLE', 'N/A')}")
-                st.write(f"Phone (Cell): {data.get('TEL;TYPE=CELL', 'N/A')}")
-                st.write(f"Phone (Work): {data.get('TEL;TYPE=WORK', 'N/A')}")
-                st.write(f"Email: {data.get('EMAIL;TYPE=WORK', 'N/A')}")
-                st.write(f"Website: {data.get('URL', 'N/A')}")
-                st.write(f"LinkedIn: {data.get('X-SOCIALPROFILE', 'N/A')}")
+    if vcard_data:
+        # Display the vCard details
+        st.write(f"Name: {vcard_data.get('FN', 'N/A')}")
+        st.write(f"Organization: {vcard_data.get('ORG', 'N/A')}")
+        st.write(f"Role: {vcard_data.get('ROLE', 'N/A')}")
+        st.write(f"Phone (Cell): {vcard_data.get('TEL;TYPE=CELL', 'N/A')}")
+        st.write(f"Phone (Work): {vcard_data.get('TEL;TYPE=WORK', 'N/A')}")
+        st.write(f"Email: {vcard_data.get('EMAIL;TYPE=WORK', 'N/A')}")
+        st.write(f"Website: {vcard_data.get('URL', 'N/A')}")
+        st.write(f"LinkedIn: {vcard_data.get('X-SOCIALPROFILE', 'N/A')}")
 
-                # Fetch and display the QR code image
-                qr_url = data.get("QR_URL")
-                if qr_url:
-                    # Create a Firebase Storage client
-                    bucket = storage.bucket()
-                    blob = bucket.blob(qr_url)  # Adjust this if the `qr_url` is not the exact path in the storage bucket
-                    qr_image_url = blob.generate_signed_url(timedelta(seconds=300), method='GET')  # This URL will be valid for 5 minutes
-                    st.image(qr_image_url, caption="QR Code", use_column_width=True)
-                    
-                st.write("---")  # Separator
-            else:
-                st.error(f"Unexpected data format for {full_name}: {data}")
+        # Display the QR code image
+        qr_url = vcard_data.get("QR_URL")
+        if qr_url:
+            st.image(qr_url, caption="QR Code", use_column_width=True)
 
+        st.write("---")  # Separator
     else:
-        st.write("No vCards found for this user.")
+        st.write("No vCard found for this user.")
 
 
 
