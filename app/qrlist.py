2dfd24392f84c47fe85e76cd22b98bf86abe1b5e
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-10-26 12:51:40 -0300

    Update qrlist.py

--- app/qrlist.py
+++ app/qrlist.py
@@ -6,40 +6,27 @@ def display_list():
 
     # Fetch the vCards for the authenticated user from Firestore
     db = firestore.client()
-    vcard_ref = db.collection('vcards').document(st.session_state.username).get()
-    vcards = vcard_ref.to_dict()
+    vcard = db.collection('vcards').document(st.session_state.username).get().to_dict()
 
-    # Print the fetched data to console
-    st.write("Fetched Data:", vcards)
-
-    if vcards and isinstance(vcards, dict):
-        # Create a list to store table data
-        table_data = []
-
-        for full_name, data in vcards.items():
-            if isinstance(data, dict):
-                row_data = {
-                    "Name": full_name,
-                    "Organization": data.get('ORG', 'N/A'),
-                    "Role": data.get('ROLE', 'N/A'),
-                    "Phone (Cell)": data.get('TEL;TYPE=CELL', 'N/A'),
-                    "Phone (Work)": data.get('TEL;TYPE=WORK', 'N/A'),
-                    "Email": data.get('EMAIL;TYPE=WORK', 'N/A'),
-                    "Website": data.get('URL', 'N/A'),
-                    "LinkedIn": data.get('X-SOCIALPROFILE', 'N/A')
-                }
-                table_data.append(row_data)
-
-                # Display the QR code image with an option to expand
-                st.image(data.get("QR_URL"), caption=f"QR Code for {full_name}", use_column_width=True)
-                if st.checkbox(f"Expand QR for {full_name}"):
-                    st.image(data.get("QR_URL"), use_column_width='auto')
-                st.write("---")  # Separator
-
-        # Display table
-        st.table(table_data)
-
+    if vcard:
+        # Display the vCard details
+        st.write(f"Name: {vcard.get('FN', 'N/A')}")
+        st.write(f"Organization: {vcard.get('ORG', 'N/A')}")
+        st.write(f"Role: {vcard.get('ROLE', 'N/A')}")
+        st.write(f"Phone (Cell): {vcard.get('TEL;TYPE=CELL', 'N/A')}")
+        st.write(f"Phone (Work): {vcard.get('TEL;TYPE=WORK', 'N/A')}")
+        st.write(f"Email: {vcard.get('EMAIL;TYPE=WORK', 'N/A')}")
+        st.write(f"Website: {vcard.get('URL', 'N/A')}")
+        st.write(f"LinkedIn: {vcard.get('X-SOCIALPROFILE', 'N/A')}")
+        
+        # Display the QR code image (ensure that "QR_URL" always exists in the vcard)
+        qr_url = vcard.get("QR_URL")
+        if qr_url:
+            st.image(qr_url, caption="QR Code", use_column_width=True)
+            
+        st.write("---")  # Separator
     else:
-        st.write("No vCards found or unexpected data format.")
+        st.write("No vCards found for this user.")
+
 
 
