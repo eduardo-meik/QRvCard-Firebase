436536f49c3f2b6cf15196e31d438589dc4f2314
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-10-29 20:46:44 -0300

    Actualizar qrvcard.py

--- app/qrvcard.py
+++ app/qrvcard.py
@@ -4,6 +4,12 @@ import io
 import base64
 from PIL import Image
 from firebase_admin import storage, firestore
+import os
+
+def b64_image(filename):
+    with open(filename, 'rb') as f:
+        b64 = base64.b64encode(f.read())
+        return b64.decode('utf-8')
 
 def generate_qr(vcard_data):
     qr = qrcode.QRCode(
@@ -125,71 +131,83 @@ def display_qr():
         img = Image.open(io.BytesIO(image_bytes))
         st.image(img, caption='Saved Profile Image', use_column_width=True)
 
-  # Populate input fields with saved data or default values
-    full_name = st.text_input("Nombre Completo", saved_vCard.get("FN", "Juan Soto"))
-    last_name, first_name = full_name.split(' ', 1) if ' ' in full_name else (full_name, '')
-    organization = st.text_input("Organización", saved_vCard.get("ORG", "Ejemplo Ltda."))
-    title = st.text_input("Puesto", saved_vCard.get("TITLE", "CEO"))
-    role = st.text_input("Rol", saved_vCard.get("ROLE", "Manager"))
-    phone_cell = st.text_input("Teléfono (Celular)", saved_vCard.get("TEL;TYPE=CELL", "(555) 555-5555"))
-    phone_work = st.text_input("Teléfono (Trabajo)", saved_vCard.get("TEL;TYPE=WORK", "(555) 555-5555"))
-    email = st.text_input("Email (Trabajo)", saved_vCard.get("EMAIL;TYPE=WORK", "john.smith@example.com"))
-    url = st.text_input("Website", saved_vCard.get("URL", "https://www.example.com"))
-    linkedin = st.text_input("LinkedIn", saved_vCard.get("X-SOCIALPROFILE", "https://www.linkedin.com/in/juansoto/"))
+  # Populateimport streamlit as st
+import base64
+import os
 
-    # Construct vCard dictionary
-    vCard = {
-        "BEGIN": "VCARD",
-        "VERSION": "3.0",
-        "KIND": "INDIVIDUAL",
-        "FN": full_name,
-        "N": f"{last_name};{first_name};;;",
-        "EMAIL;TYPE=WORK": email,
-        "TITLE": title,
-        "ROLE": role,
-        "TEL;TYPE=CELL": phone_cell,
-        "TEL;TYPE=WORK": phone_work,
-        "URL": url,
-        "ORG": organization,
-        "X-SOCIALPROFILE": linkedin,
-        "END": "VCARD",
-    }
+def b64_image(filename):
+    with open(filename, 'rb') as f:
+        b64 = base64.b64encode(f.read())
+        return b64.decode('utf-8')
 
-    # Handle image upload
-    uploaded_image = st.file_uploader("Upload your image", type=['png', 'jpg', 'jpeg'])
+# Populate input fields with saved data or default values
+full_name = st.text_input("Nombre Completo", saved_vCard.get("FN", "Juan Soto"))
+last_name, first_name = full_name.split(' ', 1) if ' ' in full_name else (full_name, '')
+organization = st.text_input("Organización", saved_vCard.get("ORG", "Ejemplo Ltda."))
+title = st.text_input("Puesto", saved_vCard.get("TITLE", "CEO"))
+role = st.text_input("Rol", saved_vCard.get("ROLE", "Manager"))
+phone_cell = st.text_input("Teléfono (Celular)", saved_vCard.get("TEL;TYPE=CELL", "(555) 555-5555"))
+phone_work = st.text_input("Teléfono (Trabajo)", saved_vCard.get("TEL;TYPE=WORK", "(555) 555-5555"))
+email = st.text_input("Email (Trabajo)", saved_vCard.get("EMAIL;TYPE=WORK", "john.smith@example.com"))
+url = st.text_input("Website", saved_vCard.get("URL", "https://www.example.com"))
+linkedin = st.text_input("LinkedIn", saved_vCard.get("X-SOCIALPROFILE", "https://www.linkedin.com/in/juansoto/"))
 
-    if uploaded_image:
-        img = Image.open(uploaded_image)
-        buffered = io.BytesIO()
-        img.save(buffered, format=img.format)
+# Handle image upload
+uploaded_image = st.file_uploader("Upload your image", type=['png', 'jpg', 'jpeg'])
 
-        # Convert image to base64 and add to the vCard
-        image_encoded = base64.b64encode(buffered.getvalue()).decode('utf-8')
-        if img.format == "JPEG":
-            vCard["PHOTO;TYPE=JPEG;ENCODING=B"] = image_encoded
-        elif img.format == "PNG":
-            vCard["PHOTO;TYPE=PNG;ENCODING=B"] = image_encoded
+if uploaded_image:
+    # Save uploaded image to a temporary file
+    tmp_filename = "tmp_uploaded_image." + uploaded_image.type.split("/")[-1]
+    with open(tmp_filename, "wb") as f:
+        f.write(uploaded_image.getvalue())
+    
+    # Encode image to base64 and add to the vCard
+    image_encoded = b64_image(tmp_filename)
+    if uploaded_image.type == "image/jpeg":
+        vCard["PHOTO;ENCODING=B;TYPE=IMAGE/JPEG"] = image_encoded
+    elif uploaded_image.type == "image/png":
+        vCard["PHOTO;ENCODING=B;TYPE=IMAGE/PNG"] = image_encoded
+    
+    # Optional: Remove the temporary file after processing
+    os.remove(tmp_filename)
 
-    # Construct vCard data string
-    vcard_data = "\n".join(f"{key}:{value}" for key, value in vCard.items())
+# Construct vCard dictionary
+vCard = {
+    "BEGIN": "VCARD",
+    "VERSION": "3.0",
+    "KIND": "INDIVIDUAL",
+    "FN": full_name,
+    "N": f"{last_name};{first_name};;;",
+    "EMAIL;TYPE=WORK": email,
+    "TITLE": title,
+    "ROLE": role,
+    "TEL;TYPE=CELL": phone_cell,
+    "TEL;TYPE=WORK": phone_work,
+    "URL": url,
+    "ORG": organization,
+    "X-SOCIALPROFILE": linkedin,
+    "END": "VCARD",
+}
 
-    if st.button('Generate QR Code'):
-        img_pil = generate_qr(vcard_data)
-        buffered = io.BytesIO()
-        img_pil.save(buffered, format="PNG")
-        img_bytes = buffered.getvalue()
+vcard_data = "\n".join(f"{key}:{value}" for key, value in vCard.items())
 
-        filename = f"{full_name}.png"
-        file_url = upload_to_firebase(img_bytes, filename)
+if st.button('Generate QR Code'):
+    img_pil = generate_qr(vcard_data)
+    buffered = io.BytesIO()
+    img_pil.save(buffered, format="PNG")
+    img_bytes = buffered.getvalue()
 
-        vCard["QR_URL"] = file_url
-        vcard_ref.set(vCard, merge=True)
+    filename = f"{full_name}.png"
+    file_url = upload_to_firebase(img_bytes, filename)
 
-        st.image(img_bytes, caption='Generated QR Code', use_column_width=True)
-        st.download_button(
-            label="Download QR Code",
-            data=img_bytes,
-            file_name=filename,
-            mime="image/png"
-        )
-        st.write(f"Uploaded to Firebase Storage: [Link]({file_url})")
+    vCard["QR_URL"] = file_url
+    vcard_ref.set(vCard, merge=True)
+
+    st.image(img_bytes, caption='Generated QR Code', use_column_width=True)
+    st.download_button(
+        label="Download QR Code",
+        data=img_bytes,
+        file_name=filename,
+        mime="image/png"
+    )
+    st.write(f"Uploaded to Firebase Storage: [Link]({file_url})")
