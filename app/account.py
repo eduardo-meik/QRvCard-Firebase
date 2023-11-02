d53864b47978b0cdc6022fbacfe3dd0226c4ef1d
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-11-02 15:04:50 -0300

    Update account.py

--- app/account.py
+++ app/account.py
@@ -3,67 +3,63 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, auth, storage
 
+
 def signout():
-    st.session_state.signout = False
-    st.session_state.signedout = False   
-    st.session_state.username = ''
-    st.session_state.login_successful = False  # Reset the login_successful flag
-    
+    st.session_state['signedout'] = True
+    st.session_state['username'] = ''
+    st.session_state['login_successful'] = False
+    st.experimental_rerun()
+
+def login_user(email, password):
+    try:
+        user = auth.get_user_by_email(email)
+        st.session_state['username'] = user.uid
+        st.session_state['useremail'] = user.email
+        st.session_state['signedout'] = False
+        st.session_state['login_successful'] = True
+    except firebase_admin.auth.AuthError:
+        st.error('Login Failed')
+        st.session_state['login_successful'] = False
+
+def create_account(email, password):
+    try:
+        user = auth.create_user(email=email, password=password)
+        st.success('Cuenta creada exitosamente. Por favor, ingrese con su email y contraseña.')
+    except Exception as e:
+        st.error(f"Error creating user: {e}")
+
 def account():
     st.title('Tarjeta de presentación en QR')
 
-    if 'username' not in st.session_state:
-        st.session_state.username = ''
-    if 'useremail' not in st.session_state:
-        st.session_state.useremail = ''
+    # If the user is signed out, clear the session
+    if 'signedout' in st.session_state and st.session_state['signedout']:
+        signout()
 
-    def f():
-        try:
-            user = auth.get_user_by_email(email)
-            st.session_state.username = user.uid
-            st.session_state.useremail = user.email
-            st.session_state.signedout = True
-            st.session_state.signout = True
-            st.session_state.login_successful = True  # Set a new session state variable
-        except:
-            st.warning('Login Failed')
-            st.session_state.login_successful = False
+    # Display signout button if the user is logged in
+    if st.session_state.get('login_successful'):
+        if st.button('Sign out'):
+            signout()
 
-
-    def t():
-        st.session_state.signout = False
-        st.session_state.signedout = False   
-        st.session_state.username = ''
-
-    if "signedout" not in st.session_state:
-        st.session_state["signedout"] = False
-    if 'signout' not in st.session_state:
-        st.session_state['signout'] = False    
-
-    if not st.session_state["signedout"]: 
+    # If the user is not logged in, display login and signup options
+    else:
         login_tab, signup_tab = st.tabs(['Login', 'Sign up'])
 
         with login_tab:
             email = st.text_input('Email Address', key='login_email')
             password = st.text_input('Password', type='password', key='login_password')
-            if st.button('Login'):
-                f()
+            if st.button('Login', on_click=login_user, args=(email, password)):
+                # Button callback handles login
+                pass
 
         with signup_tab:
-            email = st.text_input('Email Address', key='signup_email')
-            password = st.text_input('Password', type='password', key='signup_password')
+            signup_email = st.text_input('Email Address', key='signup_email')
+            signup_password = st.text_input('Password', type='password', key='signup_password')
             confirm_password = st.text_input('Confirm Password', type='password', key='signup_confirm_password')
             if st.button('Crear cuenta'):
-                if password == confirm_password:
-                    try:
-                        user = auth.create_user(email=email, password=password)
-                        st.success('Cuenta creada exitosamente')
-                        st.markdown('Ingresar con email y  password') 
-                    except Exception as e:
-                        st.error(f"Error creating user: {e}")
+                if signup_password == confirm_password:
+                    create_account(signup_email, signup_password)
                 else:
-                    st.error("Passwords incorrecto")
-
+                    st.error("Las contraseñas no coinciden.")
 
 
 
